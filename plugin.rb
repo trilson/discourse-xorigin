feb53074dc39f04198e34c681eef01c0557464c7
Tom <tom@muselive.com>
2015-05-15 23:00:45 +0100

    add support for Access-Control

--- plugin.rb
+++ plugin.rb
@@ -4,3 +4,6 @@
 # authors: Tom
 
 Rails.application.config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
+Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Origin' => 'http://www.muselive.com'})
+Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Methods' => 'GET, POST, OPTIONS, DELETE'})
+Rails.application.config.action_dispatch.default_headers.merge!({'Access-Control-Allow-Headers' => 'Content-Type, Authorization, X-Requested-With'})
